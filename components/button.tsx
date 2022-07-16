5e96c1134496f2ce5a99c1f5d7ec81439eb49bb7
Romi Kusuma Bakti <romikusumab@gmail.com>
2022-07-17 01:49:44 +0700

    fix types

--- components/button.tsx
+++ components/button.tsx
@@ -1,16 +1,28 @@
-const Button = ({ children, className, variant, size }: any) => {
-  const variants = {
+interface VariantStyle {
+  button: string;
+  stateLayer: string;
+}
+
+interface Prop {
+  children: any;
+  className: string;
+  variant: string;
+  size: string;
+}
+
+const Button = ({ children, className, variant, size }: Prop) => {
+  const variants: Record<string, VariantStyle> = {
     filled: {
       button: "bg-primary text-on-primary",
-      stateLayer: "bg-on-primary"
+      stateLayer: "bg-on-primary",
     },
     tonal: {
       button: "bg-secondary-container text-on-secondary-container",
-      stateLayer: "bg-on-secondary-container"
+      stateLayer: "bg-on-secondary-container",
     },
   };
 
-  const sizes = {
+  const sizes: Record<string, string> = {
     small: "text-xs h-8 px-4",
     medium: "text-sm h-10 px-6",
     large: "text-md h-12 px-8",
@@ -22,9 +34,11 @@ const Button = ({ children, className, variant, size }: any) => {
         variants[variant || "filled"].button
       } ${sizes[size || "medium"]} ${className}`}
     >
-      <div className={`absolute top-0 left-0 w-full h-full rounded-full opacity-0 ${
-        variants[variant || "filled"].stateLayer
-      } group-hover:opacity-hover group-focus:opacity-focus`}></div>
+      <div
+        className={`absolute top-0 left-0 w-full h-full rounded-full opacity-0 ${
+          variants[variant || "filled"].stateLayer
+        } group-hover:opacity-hover group-focus:opacity-focus`}
+      ></div>
       {children}
     </button>
   );
