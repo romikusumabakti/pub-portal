40bdb8d8d50697489fead4b5f852220aae8b54d4
Romi Kusuma Bakti <romikusumab@gmail.com>
2022-07-21 06:26:30 +0700

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,25 +3,9 @@ import "../styles/prism-vsc-dark-plus.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
-import { NextPage } from "next";
-import { ReactElement, ReactNode } from "react";
 
-export type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
-
-function MyApp({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppPropsWithLayout) {
-  // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout ?? ((page) => page);
-
-  return getLayout(
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
     <SessionProvider session={session}>
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
