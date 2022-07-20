e28e0d812069b2cd755e6bf083bc5e3a10ef04d3
Romi Kusuma Bakti <romikusumab@gmail.com>
2022-07-21 06:26:30 +0700

    add google analytics

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-34P0PHZW2P"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-34P0PHZW2P');`}
+        </Script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
