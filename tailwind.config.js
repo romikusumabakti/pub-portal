40f7a9cc23bfcb0c8b3a59fee1745654a06b2bb3
Romi Kusuma Bakti <romikusumab@gmail.com>
2022-07-31 08:02:52 +0700

    add generation icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-const colors = {
-  blue: "#2196F3",
-  green: "#4CAF50",
-  yellow: "#FFEB3B",
-  orange: "FF9800",
-  red: "#f44336",
-};
-const googleColors = {
-  blue: "#4285F4",
-  purple: "#966494",
-  red: "#EA4335",
-  yellow: "#EA8600",
-  green: "#34A853",
-  teal: "#3B96A3",
-};
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
